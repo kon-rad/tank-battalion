69b24c0d85cc702b9bf9a6745adaac7cf145ceac
konradgnat <konradmgnat@gmail.com>
2018-01-27 13:26:11 -0600

    add nes controller

--- src/js/modules/util.js
+++ src/js/modules/util.js
@@ -1,12 +1,13 @@
 'use strict';
 
-const info_button = document.getElementsByClassName('controls__icon')[0];
-const info_content = document.getElementsByClassName('controls__content')[0];
+// old onclick info button on right panel
+// const info_button = document.getElementsByClassName('controls__icon')[0];
+// const info_content = document.getElementsByClassName('controls__content')[0];
 
-info_button.onclick = () => {
-	if(info_content.style.display == 'inline-block') {
-		info_content.style.display = 'none';
-	} else {
-		info_content.style.display = 'inline-block';
-	}
-};
+// info_button.onclick = () => {
+// 	if(info_content.style.display == 'inline-block') {
+// 		info_content.style.display = 'none';
+// 	} else {
+// 		info_content.style.display = 'inline-block';
+// 	}
+// };
