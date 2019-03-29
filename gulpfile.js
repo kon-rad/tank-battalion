5bdfaab14cc71750b87e96b73240c81b88e9d0fb
konradgnat <konradmgnat@gmail.com>
2019-03-28 21:28:40 -0500

    better

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,98 @@
 'use strict';
- 
-var gulp            = require('gulp'),
-    babel           = require('gulp-babel'),
-    sass            = require('gulp-sass'),
-    cssnano         = require('gulp-cssnano'),
-    sourcemaps      = require('gulp-sourcemaps'),
-    imagemin        = require('gulp-imagemin'),
-    autoprefixer    = require('gulp-autoprefixer');
- 
+
+var gulp = require('gulp'),
+    babel = require('gulp-babel'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    sourcemaps = require('gulp-sourcemaps'),
+    imagemin = require('gulp-imagemin'),
+    autoprefixer = require('gulp-autoprefixer');
+
 gulp.task('babel', () =>
-    gulp.src('src/js/**/*.js')
-        .pipe(babel({
-            presets: ['env']
-        }))
+    gulp
+        .src('src/js/**/*.js')
+        .pipe(
+            babel({
+                presets: ['env']
+            })
+        )
         .pipe(gulp.dest('public/js/'))
 );
 
-gulp.task('workflow', function () {
-  gulp.src('./src/stylesheets/**/*.scss')
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
-      cascade: false,
-    }))
-    .pipe(cssnano())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./public/stylesheets/'));
+gulp.task('workflow', function() {
+    gulp.src('./src/stylesheets/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(
+            autoprefixer({
+                browsers: ['last 2 versions'],
+                cascade: false
+            })
+        )
+        .pipe(cssnano())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./public/stylesheets/'));
 });
 
 gulp.task('copyfonts', () =>
-gulp.src('./src/assets/fonts/*.{ttf,woff,eof,svg}')
-    .pipe(gulp.dest('./public/assets/fonts')))
- 
+    gulp
+        .src('./src/assets/fonts/*.{ttf,woff,eof,svg}')
+        .pipe(gulp.dest('./public/assets/fonts'))
+);
+
 gulp.task('moveFavicon', () =>
-gulp.src('src/favicon/**.*')
-    .pipe(gulp.dest('public/favicon/'))
+    gulp.src('src/favicon/**.*').pipe(gulp.dest('public/favicon/'))
 );
 
 gulp.task('imagemin', () => {
     gulp.src('./src/assets/images/*')
-      .pipe(imagemin())
-      .pipe(gulp.dest('./public/assets/images'))
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/assets/images'));
 });
 
 gulp.task('moveJSVendor', () => {
-    gulp.src('src/js-vendor/require.js')
-      .pipe(gulp.dest('public/js'));
-    gulp.src('src/js-vendor/socket.io.js')
-      .pipe(gulp.dest('public/'));
-  }
-);
+    gulp.src('src/js-vendor/require.js').pipe(gulp.dest('public/js'));
+    gulp.src('src/js-vendor/socket.io.js').pipe(gulp.dest('public/'));
+});
 
 gulp.task('moveAudio', () => {
-    gulp.src('src/assets/audio/*')
-      .pipe(gulp.dest('public/assets/audio'));
-  }
+    gulp.src('src/assets/audio/*').pipe(gulp.dest('public/assets/audio'));
+});
+
+gulp.task(
+    'default',
+    [
+        'workflow',
+        'babel',
+        'copyfonts',
+        'moveFavicon',
+        'imagemin',
+        'moveJSVendor',
+        'moveAudio'
+    ],
+    function() {
+        gulp.watch('src/stylesheets/**/*.scss', ['workflow']);
+        gulp.watch('./src/js/**/*.js', ['babel']);
+    }
 );
 
-gulp.task('default', ['workflow', 'babel', 'copyfonts', 'moveFavicon', 'imagemin', 'moveJSVendor', 'moveAudio'], function () {
-  gulp.watch('src/stylesheets/**/*.scss', ['workflow']);
-  gulp.watch('./src/js/**/*.js', ['babel']);
+gulp.task('dev', ['workflow', 'babel'], function() {
+    gulp.watch('src/stylesheets/**/*.scss', ['workflow']);
+    gulp.watch('./src/js/**/*.js', ['babel']);
 });
 
-gulp.task('dev', ['workflow', 'babel'], function () {
-  gulp.watch('src/stylesheets/**/*.scss', ['workflow']);
-  gulp.watch('./src/js/**/*.js', ['babel']);
-});
-
-gulp.task('build', ['workflow', 'babel', 'copyfonts', 'moveFavicon', 'imagemin', 'moveJSVendor', 'moveAudio'], function () {
-
-  console.log('Application ready! God speed!')
-});
+gulp.task(
+    'build',
+    [
+        'workflow',
+        'babel',
+        'copyfonts',
+        'moveFavicon',
+        'imagemin',
+        'moveJSVendor',
+        'moveAudio'
+    ],
+    function() {
+        console.log('Application ready! God speed!');
+    }
+);
