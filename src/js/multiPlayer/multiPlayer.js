888ef51e17399e05c0b278136dc9501ce0dd3b97
konradgnat <konradmgnat@gmail.com>
2019-07-10 19:33:55 -0500

    improve code

--- src/js/multiPlayer/multiPlayer.js
+++ src/js/multiPlayer/multiPlayer.js
@@ -7,12 +7,13 @@ define(['game', 'audio', 'mWorld', 'tank', 'multiPlayer_draw'], function(
     tank,
     multiPlayer_draw
 ) {
+    const MULTIPLAYER_DISPLAY = document.getElementById('mpS');
+
     const init = () => {
         multiPlayer_draw.start();
 
         game.socket.on('player-disconnected', () => {
-            const mpDisplay = document.getElementById('mpS');
-            mpDisplay.innerHTML = '';
+            MULTIPLAYER_DISPLAY.innerHTML = '';
         });
 
         game.socket.on('send-game-state', gameState => {
@@ -28,37 +29,27 @@ define(['game', 'audio', 'mWorld', 'tank', 'multiPlayer_draw'], function(
     };
 
     const displayMultiplayer = () => {
-        const mpDisplay = document.getElementById('mpS');
-
         for (let key in game.users) {
             let user = game.users[key];
             let userId = 'mpS_' + user.id;
             let userIdScore = 'mpS_' + user.id + '_score';
             let userIdLives = 'mpS_' + user.id + '_lives';
             let userDisplay = '';
+            const USER_SCORE_ELEMENT = document.getElementById(userIdScore);
+            const USER_LIVES_ELEMENT = document.getElementById(userIdLives);
+            const USER_SCORE = parseInt(USER_SCORE_ELEMENT.innerHTML);
+            const USER_LIVES = parseInt(USER_LIVES_ELEMENT.innerHTML);
             if (document.getElementById(userId)) {
-                if (
-                    parseInt(document.getElementById(userIdScore).innerHTML) !==
-                    user.points
-                ) {
-                    document.getElementById(userIdScore).innerHTML =
-                        user.points;
+                if (USER_SCORE !== user.points) {
+                    USER_SCORE_ELEMENT.innerHTML = user.points;
                 }
 
-                if (
-                    parseInt(document.getElementById(userIdLives).innerHTML) !==
-                    user.lives
-                ) {
-                    if (user.lives < 0) {
-                        mpDisplay.innerHTML = '';
-                    } else {
-                        document.getElementById(userIdLives).innerHTML =
-                            user.lives;
-                    }
+                if (USER_LIVES !== user.lives && user.lives >= 0) {
+                    USER_LIVES_ELEMENT.innerHTML = user.lives;
                 }
 
                 if (user.lives < 0) {
-                    mpDisplay.innerHTML = '';
+                    MULTIPLAYER_DISPLAY.innerHTML = '';
                 }
             } else {
                 userDisplay =
@@ -76,7 +67,7 @@ define(['game', 'audio', 'mWorld', 'tank', 'multiPlayer_draw'], function(
                     '"> ' +
                     user.lives +
                     '</span></div></div></div>';
-                mpDisplay.innerHTML += userDisplay;
+                MULTIPLAYER_DISPLAY.innerHTML += userDisplay;
             }
         }
     };
