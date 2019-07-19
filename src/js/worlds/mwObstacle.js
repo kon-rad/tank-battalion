00a0f9d812ad86792c70ee307fc8ab6a9bcd67ce
konradgnat <konradmgnat@gmail.com>
2019-07-18 21:42:28 -0500

    reformat code

--- src/js/worlds/mwObstacle.js
+++ src/js/worlds/mwObstacle.js
@@ -7,27 +7,27 @@ define([], () => {
         if (x <= 0 || x >= 60 || y <= 0 || y >= 60) {
             return true;
         }
-        let row = world[y].split('');
-        let pos = Number(row[x]),
-            pos2,
-            pos3;
-        if (dir === 'up' || dir === 'down') {
-            pos2 = Number(row[x - 1]);
-            pos3 = Number(row[x + 1]);
-        } else if (dir === 'right' || dir === 'left') {
-            let row2 = world[y - 1];
-            row2 = row2.split('');
-            let row3 = world[y + 1];
-            row3 = row3.split('');
-            pos2 = Number(row2[x]);
-            pos3 = Number(row3[x]);
+        const row = world[y].split('');
+        let pos = Number(row[x]), pos2, pos3;
+
+        switch (dir) {
+            case 'up':
+            case 'down':
+                pos2 = Number(row[x - 1]);
+                pos3 = Number(row[x + 1]);
+                break;
+            case 'right':
+            case 'left':
+                let row2 = world[y - 1];
+                row2 = row2.split('');
+                let row3 = world[y + 1];
+                row3 = row3.split('');
+                pos2 = Number(row2[x]);
+                pos3 = Number(row3[x]);
+                break;
         }
 
-        if (pos || pos2 || pos3) {
-            return true;
-        }
-
-        return false;
+        return !!pos || !!pos2 || !!pos3;
     };
 
     return {
