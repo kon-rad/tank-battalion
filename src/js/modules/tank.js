f46841bcdf6b312edf29115783a4c25eefb423da
konradgnat <konradmgnat@gmail.com>
2020-06-27 23:41:36 -0500

    improve tank code

--- src/js/modules/tank.js
+++ src/js/modules/tank.js
@@ -3,10 +3,7 @@ import game from 'modules/game';
 const tank = {};
 const GREEN = 'green';
 
-tank.moving_up = (x, y, color) => {
-    if (color === undefined) {
-        color = GREEN;
-    }
+tank.moving_up = (x, y, color = GREEN) => {
     game.context.fillStyle = color;
     game.context.fillRect(x - 8, y - 8, 14, 14);
     game.context.fillRect(x - 2, y - 18, 4, 12);
@@ -15,10 +12,7 @@ tank.moving_up = (x, y, color) => {
     game.context.fillRect(x + 6, y - 12, 6, 24);
 };
 
-tank.moving_down = (x, y, color) => {
-    if (color === undefined) {
-        color = GREEN;
-    }
+tank.moving_down = (x, y, color = GREEN) => {
     game.context.fillStyle = color;
     game.context.fillRect(x - 8, y - 7, 14, 14);
     game.context.fillRect(x - 2, y + 6, 4, 12);
@@ -27,10 +21,7 @@ tank.moving_down = (x, y, color) => {
     game.context.fillRect(x + 6, y - 12, 6, 24);
 };
 
-tank.moving_right = (x, y, color) => {
-    if (color === undefined) {
-        color = GREEN;
-    }
+tank.moving_right = (x, y, color = GREEN) => {
     game.context.fillStyle = color;
     game.context.fillRect(x - 7, y - 6, 14, 14);
     game.context.fillRect(x + 7, y - 2, 13, 4);
@@ -39,10 +30,7 @@ tank.moving_right = (x, y, color) => {
     game.context.fillRect(x - 12, y + 8, 24, 6);
 };
 
-tank.moving_left = (x, y, color) => {
-    if (color === undefined) {
-        color = GREEN;
-    }
+tank.moving_left = (x, y, color = GREEN) => {
     game.context.fillStyle = color;
     game.context.fillRect(x - 8, y - 6, 14, 14);
     game.context.fillRect(x - 20, y - 2, 12, 4);
